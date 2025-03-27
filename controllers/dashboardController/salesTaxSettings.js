b6994c6778bdd347e37a008f9cce796fdf75a242
struggle-Strug <strugstruggle0430@gmail.com>
2025-03-28 01:59:18 +0900

    fix :fix test masters

--- controllers/dashboardController/salesTaxSettings.js
+++ controllers/dashboardController/salesTaxSettings.js
@@ -1,8 +1,8 @@
 module.exports = (db) => {
-    return {
-      // Initialize the database table
-      init: (req, res) => {
-        const sql = `
+  return {
+    // Initialize the database table
+    init: (req, res) => {
+      const sql = `
             CREATE TABLE IF NOT EXISTS taxes (
                 id INT AUTO_INCREMENT PRIMARY KEY, -- 消費税コード（主キー・自動増分）
                 tax_rate INT NOT NULL, -- 消費税率（null不可）
@@ -12,77 +12,76 @@ module.exports = (db) => {
                 created TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 作成日時（null可、デフォルトは現在の日時）
             );
         `;
-  
-        db.query(sql, (err) => {
-          if (err) {
-            console.error("Error creating salesTaxes table:", err.message);
-            return res.status(500).send("Error initializing salesTaxes table.");
-          }
-          res.send("SalesTaxes table initialized successfully.");
-        });
-      },
 
-      // Load salesTaxes
-      load: (req, res) => {
-        const sql = "SELECT * FROM taxes";
-        db.query(sql, (err, rows) => {
-          if (err) {
-            console.error("Error loading taxes:", err.message);
-            return res.status(500).send("Error loading taxes.");
-          }
-          res.json(rows);
-        });
-      },
-
-      //Get posCoordinationSetting By ID
-      getById: (req, res) => {
-        const id = req.params.id;
-        const sql = "SELECT * FROM taxes WHERE id = ?";
-        db.query(sql, [id], (err, rows) => {
-          if (err) {
-            console.error("Error fetching taxes:", err.message);
-            return res.status(500).send("Error fetching taxes.");
-          }
-          res.json(rows[0]);
-        });
-      },
-
-      //Save
-      save: (req, res) => {
-        const { id, tax_rate, start_date, end_date } = req.body;
-        let sql, params;
-
-        if (id) {
-          // Update existing admin setting
-          sql = `UPDATE taxes SET tax_rate = ?, start_date = ?, end_date = ?, updated = NOW() WHERE id = ?`;
-          params = [tax_rate, start_date, end_date, id];
-        } else {
-          // Insert new admin setting
-          sql = `INSERT INTO taxes (tax_rate, start_date, end_date, created, updated) VALUES (?, ?, ?, NOW(), NOW())`;
-          params = [tax_rate, start_date, end_date];
+      db.query(sql, (err) => {
+        if (err) {
+          console.error("Error creating salesTaxes table:", err.message);
+          return res.status(500).send("Error initializing salesTaxes table.");
         }
+        res.send("SalesTaxes table initialized successfully.");
+      });
+    },
 
-        db.query(sql, params, (err) => {
-          if (err) {
-            console.error("Error saving taxes:", err.message);
-            return res.status(500).send("Error saving taxes.");
-          }
-          res.send("Taxes saved successfully.");
-        });
-      },
+    // Load salesTaxes
+    load: (req, res) => {
+      const sql = "SELECT * FROM taxes";
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.error("Error loading taxes:", err.message);
+          return res.status(500).send("Error loading taxes.");
+        }
+        res.json(rows);
+      });
+    },
 
-      //Delete catagory
-      deleteById: (req, res) => {
-        const id = req.params.id;
-        const sql = "DELETE FROM taxes WHERE id = ?";
-        db.query(sql, [id], (err) => {
-          if (err) {
-            console.error("Error deleting taxes:", err.message);
-            return res.status(500).send("Error deleting taxes.");
-          }
-          res.send("Taxes deleted successfully.");
-        });
-      },
-    };
+    //Get posCoordinationSetting By ID
+    getById: (req, res) => {
+      const id = req.params.id;
+      const sql = "SELECT * FROM taxes WHERE id = ?";
+      db.query(sql, [id], (err, rows) => {
+        if (err) {
+          console.error("Error fetching taxes:", err.message);
+          return res.status(500).send("Error fetching taxes.");
+        }
+        res.json(rows[0]);
+      });
+    },
+
+    //Save
+    save: (req, res) => {
+      const { id, tax_rate, start_date, end_date } = req.body;
+      let sql, params;
+
+      if (id) {
+        // Update existing admin setting
+        sql = `UPDATE taxes SET tax_rate = ?, start_date = ?, end_date = ?, updated = NOW() WHERE id = ?`;
+        params = [tax_rate, start_date, end_date, id];
+      } else {
+        // Insert new admin setting
+        sql = `INSERT INTO taxes (tax_rate, start_date, end_date, created, updated) VALUES (?, ?, ?, NOW(), NOW())`;
+        params = [tax_rate, start_date, end_date];
+      }
+
+      db.query(sql, params, (err) => {
+        if (err) {
+          console.error("Error saving taxes:", err.message);
+          return res.status(500).send("Error saving taxes.");
+        }
+        res.send("Taxes saved successfully.");
+      });
+    },
+
+    //Delete catagory
+    deleteById: (req, res) => {
+      const id = req.params.id;
+      const sql = "DELETE FROM taxes WHERE id = ?";
+      db.query(sql, [id], (err) => {
+        if (err) {
+          console.error("Error deleting taxes:", err.message);
+          return res.status(500).send("Error deleting taxes.");
+        }
+        res.send("Taxes deleted successfully.");
+      });
+    },
   };
-  
+};
--- controllers/mastersController/products.js
+++ controllers/mastersController/products.js
@@ -1,16 +1,15 @@
 module.exports = (db) => {
-    return {
-      // Initialize the database table
-      init: (req, res) => {
-  
-        const sql = `
+  return {
+    // Initialize the database table
+    init: (req, res) => {
+      const sql = `
             CREATE TABLE IF NOT EXISTS products (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255) NOT NULL,
                 code VARCHAR(255) DEFAULT NULL,
                 category VARCHAR(255) DEFAULT NULL,
                 subcategory VARCHAR(255) DEFAULT NULL,
-                classification_primary VARCHAR(255) NOT NULL,
+                classification_primary VARCHAR(255) DEFAULT NULL,
                 classification_secondary VARCHAR(255) DEFAULT NULL,
                 jan_code VARCHAR(255) NOT NULL,
                 standard_retail_price INT NOT NULL,
@@ -34,47 +33,75 @@ module.exports = (db) => {
                 updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             );
         `;
-  
-        db.query(sql, (err) => {
-          if (err) {
-            console.error("Error creating products table:", err.message);
-            return res.status(500).send("Error initializing products table.");
-          }
-          res.send("Products table initialized successfully.");
-        });
-      },
 
-      //Load products
-      load: (req, res) => {
-          
-        const sql = "SELECT * FROM products";
+      db.query(sql, (err) => {
+        if (err) {
+          console.error("Error creating products table:", err.message);
+          return res.status(500).send("Error initializing products table.");
+        }
+        res.send("Products table initialized successfully.");
+      });
+    },
 
-        db.query(sql, (err, rows) => {
-          if (err) {
-            console.error("Error loading products:", err.message);
-            return res.status(500).send("Error loading products.");
-          }
-          res.json(rows);
-        });
-      },
+    //Load products
+    load: (req, res) => {
+      const sql = "SELECT * FROM products";
 
-      //Get product by ID
-      getById: (req, res) => {
-        const id = req.params.id;
-        const sql = "SELECT * FROM products WHERE id = ?";
-        db.query(sql, [id], (err, rows) => {
-          if (err) {
-            console.error("Error fetching product:", err.message);
-            return res.status(500).send("Error fetching product.");
-          }
-          res.json(rows[0]);
-        });
-      },
+      db.query(sql, (err, rows) => {
+        if (err) {
+          console.error("Error loading products:", err.message);
+          return res.status(500).send("Error loading products.");
+        }
+        res.json(rows);
+      });
+    },
 
-      //Save product
-      save: (req, res) => {
-        const {
-          id,
+    //Get product by ID
+    getById: (req, res) => {
+      const id = req.params.id;
+      const sql = "SELECT * FROM products WHERE id = ?";
+      db.query(sql, [id], (err, rows) => {
+        if (err) {
+          console.error("Error fetching product:", err.message);
+          return res.status(500).send("Error fetching product.");
+        }
+        res.json(rows[0]);
+      });
+    },
+
+    //Save product
+    save: (req, res) => {
+      const {
+        id,
+        name,
+        code,
+        category,
+        subcategory,
+        jan_code,
+        standard_retail_price,
+        procurement_cost,
+        manufacturer_name,
+        specification,
+        unit,
+        country_of_origin,
+        storage_location,
+        storage_method,
+        threshold,
+        tax_rate,
+        raw_materials,
+        raw_materials_weight,
+        raw_materials_cost,
+        raw_costItem,
+        raw_costItem_cost_weight,
+        raw_costItem_cost,
+        total_original_cost,
+      } = req.body;
+      let sql, params;
+
+      if (id) {
+        // Update existing product
+        sql = `UPDATE products SET name = ?, code = ?, category = ?, subcategory = ?, jan_code = ?, standard_retail_price = ?, procurement_cost = ?, manufacturer_name = ?, specification = ?, unit = ?, country_of_origin = ?, storage_location = ?, storage_method = ?, threshold = ?, tax_rate = ?, updated = NOW() WHERE id = ?`;
+        params = [
           name,
           code,
           category,
@@ -90,155 +117,135 @@ module.exports = (db) => {
           storage_method,
           threshold,
           tax_rate,
-          raw_materials,
+          JSON.stringify(raw_materials),
           raw_materials_weight,
           raw_materials_cost,
           raw_costItem,
           raw_costItem_cost_weight,
           raw_costItem_cost,
           total_original_cost,
-        } = req.body;
-        let sql, params;
+          id,
+        ];
+      } else {
+        // Insert new product
+        sql = `INSERT INTO products (name, code, category, subcategory, jan_code, standard_retail_price, procurement_cost, manufacturer_name, specification, unit, country_of_origin, storage_location, storage_method, threshold, tax_rate, created, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())`;
+        params = [
+          name,
+          code,
+          category,
+          subcategory,
+          jan_code,
+          standard_retail_price,
+          procurement_cost,
+          manufacturer_name,
+          specification,
+          unit,
+          country_of_origin,
+          storage_location,
+          storage_method,
+          threshold,
+          tax_rate,
+          JSON.stringify(raw_materials), // Convert materials array to JSON string
+          raw_materials_weight,
+          raw_materials_cost,
+          raw_costItem,
+          raw_costItem_cost_weight,
+          raw_costItem_cost,
+          total_original_cost,
+        ];
+      }
 
-        if (id) {
-          // Update existing product
-          sql = `UPDATE products SET name = ?, code = ?, category = ?, subcategory = ?, jan_code = ?, standard_retail_price = ?, procurement_cost = ?, manufacturer_name = ?, specification = ?, unit = ?, country_of_origin = ?, storage_location = ?, storage_method = ?, threshold = ?, tax_rate = ?, updated = NOW() WHERE id = ?`;
-          params = [
-            name,
-            code,
-            category,
-            subcategory,
-            jan_code,
-            standard_retail_price,
-            procurement_cost,
-            manufacturer_name,
-            specification,
-            unit,
-            country_of_origin,
-            storage_location,
-            storage_method,
-            threshold,
-            tax_rate,
-            JSON.stringify(raw_materials),
-            raw_materials_weight,
-            raw_materials_cost,
-            raw_costItem,
-            raw_costItem_cost_weight,
-            raw_costItem_cost,
-            total_original_cost,
-            id,
-          ];
-        } else {
-          // Insert new product
-          sql = `INSERT INTO products (name, code, category, subcategory, jan_code, standard_retail_price, procurement_cost, manufacturer_name, specification, unit, country_of_origin, storage_location, storage_method, threshold, tax_rate, created, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())`;
-          params = [
-              name,
-              code,
-              category,
-              subcategory,
-              jan_code,
-              standard_retail_price,
-              procurement_cost,
-              manufacturer_name,
-              specification,
-              unit,
-              country_of_origin,
-              storage_location,
-              storage_method,
-              threshold,
-              tax_rate,
-              JSON.stringify(raw_materials), // Convert materials array to JSON string
-              raw_materials_weight,
-              raw_materials_cost,
-              raw_costItem,
-              raw_costItem_cost_weight,
-              raw_costItem_cost,
-              total_original_cost
-          ];
+      db.query(sql, params, (err) => {
+        if (err) {
+          console.error("Error saving product:", err.message);
+          return res.status(500).send("Error saving product.");
         }
+        res.send("Product saved successfully.");
+      });
+    },
 
-        db.query(sql, params, (err) => {
-          if (err) {
-            console.error("Error saving product:", err.message);
-            return res.status(500).send("Error saving product.");
-          }
-          res.send("Product saved successfully.");
-        });
-      },
+    //Delete product
+    deleteById: (req, res) => {
+      const id = req.params.id;
+      const sql = "DELETE FROM products WHERE id = ?";
+      db.query(sql, [id], (err) => {
+        if (err) {
+          console.error("Error deleting product:", err.message);
+          return res.status(500).send("Error deleting product.");
+        }
+        res.send("Product deleted successfully.");
+      });
+    },
 
-      //Delete product
-      deleteById: (req, res) => {
-        const id = req.params.id;
-        const sql = "DELETE FROM products WHERE id = ?";
-        db.query(sql, [id], (err) => {
-          if (err) {
-            console.error("Error deleting product:", err.message);
-            return res.status(500).send("Error deleting product.");
-          }
-          res.send("Product deleted successfully.");
-        });
-      },
+    //searchId
+    searchIdProducts: (req, res) => {
+      const { id } = req.query; // Assuming the query is passed as a query parameter, e.g., ?name=example
 
-      //searchId
-      searchIdProducts: (req, res) => {
-        const { id } = req.query // Assuming the query is passed as a query parameter, e.g., ?name=example
-        
-        let sql;
-        let params = [];
-        
-        if (id.trim() !== "") {
-          sql = `
+      let sql;
+      let params = [];
+
+      if (id.trim() !== "") {
+        sql = `
               SELECT * FROM products LEFT JOIN inventories ON products.id = inventories.product_id
               WHERE products.id LIKE ?
               `;
-          params = [`%${id}%`];
-        } else {
-          sql = `SELECT * FROM products LEFT JOIN inventories ON products.id = inventories.product_id`;
-        }
-      
-        db.query(sql, params, (err, rows) => {
-          if (err) {
-            console.error("Error fetching products:", err.message);
-            return res.status(500).send("Error fetching products.");
-          }
-          res.json(rows);
-        });
-      },
+        params = [`%${id}%`];
+      } else {
+        sql = `SELECT * FROM products LEFT JOIN inventories ON products.id = inventories.product_id`;
+      }
 
-      //searchName
-      searchNameProducts: (req, res) => {
-        const { name } = req.query // Assuming the query is passed as a query parameter, e.g., ?name=example
-        
-        let sql;
-        let params = [];
-        
-        if (name.trim() !== "") {
-          sql = `
+      db.query(sql, params, (err, rows) => {
+        if (err) {
+          console.error("Error fetching products:", err.message);
+          return res.status(500).send("Error fetching products.");
+        }
+        res.json(rows);
+      });
+    },
+
+    //searchName
+    searchNameProducts: (req, res) => {
+      const { name } = req.query; // Assuming the query is passed as a query parameter, e.g., ?name=example
+
+      let sql;
+      let params = [];
+
+      if (name.trim() !== "") {
+        sql = `
               SELECT * FROM products 
               WHERE name LIKE ?
               `;
-          params = [`%${name}%`];
-        } else {
-          sql = `SELECT * FROM products`;
-        }
-      
-        db.query(sql, params, (err, rows) => {
-          if (err) {
-            console.error("Error fetching products:", err.message);
-            return res.status(500).send("Error fetching products.");
-          }
-          res.json(rows);
-        });
-      },
+        params = [`%${name}%`];
+      } else {
+        sql = `SELECT * FROM products`;
+      }
 
-      //search
-      searchProducts: (req, res) => {
-        const { name, category, subcategory, jan_code, manufacturer_name } = req.query; // Assuming the query is passed as a query parameter, e.g., ?name=example
-        let sql;
-        let params = [];
-      
-        if ((name || category || subcategory || jan_code || manufacturer_name).trim() !== "") {
-          sql = `
+      db.query(sql, params, (err, rows) => {
+        if (err) {
+          console.error("Error fetching products:", err.message);
+          return res.status(500).send("Error fetching products.");
+        }
+        res.json(rows);
+      });
+    },
+
+    //search
+    searchProducts: (req, res) => {
+      const { name, category, subcategory, jan_code, manufacturer_name } =
+        req.query; // Assuming the query is passed as a query parameter, e.g., ?name=example
+      let sql;
+      let params = [];
+
+      if (
+        (
+          name ||
+          category ||
+          subcategory ||
+          jan_code ||
+          manufacturer_name
+        ).trim() !== ""
+      ) {
+        sql = `
               SELECT * FROM products 
               WHERE name LIKE ? 
               OR category LIKE ? 
@@ -246,25 +253,24 @@ module.exports = (db) => {
               OR jan_code LIKE ? 
               OR manufacturer_name LIKE ?
               `;
-          params = [
-            name || '%',  // Fallback to '%' if the query is not provided
-            category || '%',
-            subcategory || '%',
-            jan_code || '%',
-            manufacturer_name || '%',
-          ];
-        } else {
-          sql = `SELECT * FROM products`;
+        params = [
+          name || "%", // Fallback to '%' if the query is not provided
+          category || "%",
+          subcategory || "%",
+          jan_code || "%",
+          manufacturer_name || "%",
+        ];
+      } else {
+        sql = `SELECT * FROM products`;
+      }
+
+      db.query(sql, params, (err, rows) => {
+        if (err) {
+          console.error("Error fetching products:", err.message);
+          return res.status(500).send("Error fetching products.");
         }
-      
-        db.query(sql, params, (err, rows) => {
-          if (err) {
-            console.error("Error fetching products:", err.message);
-            return res.status(500).send("Error fetching products.");
-          }
-          res.json(rows);
-        });
-      },
-    };
+        res.json(rows);
+      });
+    },
   };
-  
+};
